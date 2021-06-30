322cc37304ea9b468f7df2bede364d5c06cd704f
thiagobritto <thiagobritto.developer@gmail.com>
2021-06-30 07:41:46 -0300

    clean file model/user.js

--- src/controllers/dashController.js
+++ src/controllers/dashController.js
@@ -1,34 +1,36 @@
 
 const { ipcRenderer } = require('electron')
 const elementsDOM = require('../models/elementsDOM')()
-const user = require('../models/user')(ipcRenderer)
 
 require('dotenv').config()
 
 class DashController {
-    
-    authentication(btnAdmin, callback){
-        if (ipcRenderer.sendSync('getDataUser').super < 1){
-            elementsDOM.clearElements(btnAdmin); 
+
+    authentication(btnAdmin, callback) {
+        if (ipcRenderer.sendSync('getDataUser').super < 1) {
+            elementsDOM.clearElements(btnAdmin);
         } else {
             callback(btnAdmin)
         }
     }
-    
-    logout(){
+
+    logout() {
         ipcRenderer.send('startApp')
         ipcRenderer.send('closeWindow', 'dash')
     }
 
-    setNameUserIn(elementsName){
-        elementsDOM.setInElements(elementsName, user.name)
+    setNameUserIn(elementsName) {
+        elementsDOM.setInElements(
+            elementsName,
+            ipcRenderer.sendSync('getDataUser').userName
+        )
     }
 
-    setNameAppIn(elementsName){
+    setNameAppIn(elementsName) {
         elementsDOM.setInElements(elementsName, process.env.APPLICATION_NAME)
     }
 
-    openUserPanel(){
+    openUserPanel() {
         ipcRenderer.send('createWindowParent', {
             winName: 'users',
             parentName: 'dash',
